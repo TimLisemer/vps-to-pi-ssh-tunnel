ba8ab2a94b936bafdf8902ac78265730bf7a7b9e
Tim Lisemer <timlisemer@gmail.com>
2025-02-19 19:46:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ chmod 600 /root/.ssh/yakweide-tls-tunnel
 # Start the SSH tunnel
 sh -c "dbclient -y -N \
     -i /root/.ssh/yakweide-tls-tunnel \
-    -R 8123:localhost:8123 \
-    -L 9001:localhost:9001 \
+    -R 8123:10.0.0.2:8123 \
+    -L 9001:10.0.0.2:9001 \
     root@142.132.234.128"
