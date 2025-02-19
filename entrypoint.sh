eace1afde92b55a18f98051692dd8308e4ec4171
timlisemer <timlisemer@gmail.com>
2025-02-19 15:28:08 +0100

    Convert all files to LF line endings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # Ensure SSH key has correct permissions
 chmod 600 /root/.ssh/yakweide-tls-tunnel
 
 # Start the SSH tunnel
-exec ssh -N \
+sh -c "ssh -N \
     -i /root/.ssh/yakweide-tls-tunnel \
     -o ServerAliveInterval=60 \
     -o ExitOnForwardFailure=yes \
     -R 8123:localhost:8123 \
     -L 9001:localhost:9001 \
-    root@142.132.234.128
+    root@142.132.234.128"
