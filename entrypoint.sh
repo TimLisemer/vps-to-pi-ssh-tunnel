566c51774f5260f3d1fa5b25110352c7ac462cec
Tim Lisemer <timlisemer@gmail.com>
2025-02-23 05:50:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "starting the ssh tunnel..."
 dbclient -y -N \
     -i /root/.ssh/yakweide-tls-tunnel \
     -R *:8123:10.0.0.2:8123 \
-    -L 0.0.0.0:9001:localhost:9001 \ \
+    -L 0.0.0.0:9001:localhost:9001 \
     root@142.132.234.128
 
 echo "ssh tunnel process has ended."
