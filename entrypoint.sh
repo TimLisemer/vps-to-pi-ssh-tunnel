ffd67b79d4ae5323fbbe4df42a317467f37bba94
Tim Lisemer <timlisemer@gmail.com>
2025-02-27 14:44:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@
 echo "setting correct permissions for ssh key..."
 chmod 600 /root/.ssh/yakweide-tls-tunnel
 
-echo "starting the ssh tunnel..."
-dbclient -y -N \
-    -i /root/.ssh/yakweide-tls-tunnel \
-    -R *:8123:10.0.0.2:8123 \
-    -L 0.0.0.0:9001:localhost:9001 \
-    root@142.132.234.128
+while true; do
+    echo "starting the ssh tunnel..."
+    dbclient -y -N \
+        -i /root/.ssh/yakweide-tls-tunnel \
+        -R *:8123:10.0.0.2:8123 \
+        -L 0.0.0.0:9001:localhost:9001 \
+        root@142.132.234.128
 
-echo "ssh tunnel process has ended."
+    echo "ssh tunnel process has ended. restarting in 5 seconds..."
+    sleep 5
+done
